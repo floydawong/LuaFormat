b227fa046628f071fdc10aae192edb0e8d4a5b21
Floyda <floyda@163.com>
2017-11-07 17:53:18 +0800

    - key bingding - utest 10

---
+++ utest/lua-example/target-10.lua
@@ -0,0 +1,34 @@
+-- https://github.com/FloydaGithub/LuaFormat/issues/9
+
+--抽象类声名
+__DebugAbstract__()
+class "AbstractClass" (function(_ENV)
+    --加上__DebugRequire__()属性，子类不实现会报错
+    __DebugRequire__() 
+    function AbstractFuctionName(self, ...)
+    end
+end)
+
+--普通基类
+class "BaseClass" (function(_ENV)
+    
+    ----------------------------------------------
+    ----------------- Constructor ----------------
+    ----------------------------------------------    
+    
+    __DebugArguments__{ --[[Type]] }
+    function BaseClass(self, ...) 
+    end
+    
+    ----------------------------------------------
+    ------------------- Method -------------------
+    ----------------------------------------------
+    
+    --虚方法，约定重写的标记virtual
+    --[[virtual]]
+    __DebugArguments__{ --[[Type]] }
+    function VirtualFuctionName(self, ...)
+        -- body
+    end
+    
+end)
