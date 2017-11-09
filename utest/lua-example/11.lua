42f8bd4bddffc7269f2d617088ad9a65904997a4
Floyda <floyda@163.com>
2017-11-09 22:29:52 +0800

    - test example

---
+++ utest/lua-example/11.lua
@@ -0,0 +1,4 @@
+function  foo(a,  b)
+    if  a  >  b  then  do  return  a  end  end
+        do  return  b  end
+end
---
+++ utest/lua-example/12.lua
@@ -0,0 +1,17 @@
+--[==[
+    Hello World
+]==]
+
+function foo()
+print('Hello World')
+end
+
+--[[
+    Hello World
+]]
+
+function foo(flag)
+if flag then
+print('Hello World')
+end
+end
