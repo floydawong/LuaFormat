a0e244261469608371360d5d01928be11b07a1e4
Floyda <floyda@163.com>
2018-04-23 15:44:11 +0800

    update utest of comment

---
+++ utest/lua-example/comment.lua
@@ -0,0 +1,84 @@
+------------------------------------------------------------
+-- comment
+------------------------------------------------------------
+print("What's your name")
+-- print("What's your name")
+--[[print("What's your name")]]
+--[==[print("What's your name")]==]
+print("What's your name") -- [comment by Floyda]
+print("What's your name") --[==[print("What's your name")]==]
+
+------------------------------------------------------------
+-- [comment by Floyda] print "Hello \
+-- [comment by Floyda]    World\n"
+-- [comment by Floyda] print "\"Hello \
+-- [comment by Floyda]      World\"\n"
+
+--[[
+print "Hello \
+   World\n"
+print "\"Hello \
+     World\"\n"
+]]
+print("What's your name")
+------------------------------------------------------------
+-- [comment by Floyda] print[[
+-- [comment by Floyda]  print \n\r
+-- [comment by Floyda]   print \n
+-- [comment by Floyda]    print \a
+-- [comment by Floyda]     print \f
+-- [comment by Floyda]      print \v
+-- [comment by Floyda] ]]
+
+--[=[
+print[[
+ print \n\r
+  print \n
+   print \a
+    print \f
+     print \v
+]]
+]=]
+print("What's your name")
+------------------------------------------------------------
+-- [comment by Floyda] print '\n\r'
+-- [comment by Floyda] print '\n'
+-- [comment by Floyda] print '\a'
+-- [comment by Floyda] print '\f'
+-- [comment by Floyda] print '\v'
+
+--[[
+print '\n\r'
+print '\n'
+print '\a'
+print '\f'
+print '\v'
+]]
+print("What's your name")
+------------------------------------------------------------
+-- [comment by Floyda] print [==[
+-- [comment by Floyda] -- For
+-- [comment by Floyda] for i=1,10 do
+-- [comment by Floyda]     print(i)
+-- [comment by Floyda] end
+-- [comment by Floyda] ]==]
+
+--[[
+print [==[
+-- For
+for i=1,10 do
+    print(i)
+end
+]==]
+]]
+
+--[===[
+print [==[
+-- For
+for i=1,10 do
+    print(i)
+end
+]==]
+]===]
+print("What's your name")
+------------------------------------------------------------
