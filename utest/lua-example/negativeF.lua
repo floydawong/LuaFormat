55249911e3ff11265f9805ed58a07a6a6bb49ec1
Floyda <floyda@163.com>
2019-02-26 15:24:06 +0800

    add test

--- utest/lua-example/negativeF.lua
+++ utest/lua-example/negativeF.lua
@@ -8,3 +8,19 @@ local d=-(1+2)
 local e=1-(1+2)
 local tab={-1,-2,-3}
 local last=tab[#tab]-1
+
+function foo1()
+    return -1
+end
+
+function foo2()
+    return -1
+end
+
+function foo3()
+    return 2*-1
+end
+
+function foo4()
+    return 2*-1
+end
---
+++ utest/lua-example/target-14.lua
@@ -0,0 +1,10 @@
+function get_name()
+    return 'name'
+end
+
+local tab = {}
+table.insert(tab, {
+    index = 1,
+    uid = 1,
+    name = get_name()
+})
